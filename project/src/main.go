58d758ecf3cf7fa6c89ed9456f481d9b054a1c92
peterhdpham <peterhdpham@gmail.com>
2024-02-20 13:00:16 +0100

    fikset mappestruktur, pakker skal være en mappe med pakke inni src

--- project/src/main.go
+++ project/src/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"project/pack"
+	// "project/pack"
+	"project/udp"
 )
 
 func main() {
 
 	living_IPs := make(chan []string)
 
-	go pack.Broadcast_life()
-	go pack.Look_for_life(living_IPs)
+	go udp.Broadcast_life()
+	go udp.Look_for_life(living_IPs)
 
 	for a := range living_IPs {
 		fmt.Printf("\033[2J\033[H")
