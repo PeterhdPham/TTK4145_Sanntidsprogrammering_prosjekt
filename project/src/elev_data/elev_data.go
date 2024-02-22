ff73ab60139830e319c583223bdf2bc6e0709d78
Einar <einarbergslid@gmail.com>
2024-02-22 15:58:19 +0100

    Changed to returning the type elev_data.Elev_status, a struct for easier json formatting and sending over TCP

--- project/src/elev_data/elev_data.go
+++ project/src/elev_data/elev_data.go
@@ -2,51 +2,22 @@ package elev_data
 
 import "Driver-go/elevio"
 
-func Get_livedata2(
-	chan1 chan<- elevio.ButtonEvent,
-	chan2 chan<- int,
-	chan3 chan<- bool,
-	chan4 chan<- bool,
-) {
-
-	drv_buttons := make(chan elevio.ButtonEvent)
-	drv_floors := make(chan int)
-	drv_obstr := make(chan bool)
-	drv_stop := make(chan bool)
-
-	go elevio.PollButtons(drv_buttons)
-	go elevio.PollFloorSensor(drv_floors)
-	go elevio.PollObstructionSwitch(drv_obstr)
-	go elevio.PollStopButton(drv_stop)
-
-	for {
-		select {
-		case a := <-drv_buttons:
-			chan1 <- a
-
-		case a := <-drv_floors:
-			chan2 <- a
-
-		case a := <-drv_obstr:
-			chan3 <- a
-
-		case a := <-drv_stop:
-			chan4 <- a
-		}
-	}
+type Elev_status struct {
+	direction   int  `json:"direction"`
+	floor       int  `json:"floor"`
+	doors       bool `json:"doors"`
+	obstructed  bool `json:"obstructed"`
+	buttonfloor int  `json:"buttonfloor"`
+	buttontype  int  `json:"buttontype"`
 }
 
 func Get_livedata(
-	elev_status_chan chan<- []int,
+	elev_status_chan chan<- Elev_status,
 	direction chan elevio.MotorDirection,
 	door_open chan bool,
 ) {
 
-	elev_status := []int{0, 0, 0, 0, 0, 0}
-	// Slice contains:[direction(-1,0,1), Floor I'm at (0,1,2,3...n), Doors open? (0,1), Door obstructed? (0,1)
-	// buttonfloor(0,1,2,3...n), buttontype(0,1,2)]
-	// buttontype 0 is up, type 1 is down, type 2 is hall call
-
+	var my_status Elev_status
 	drv_buttons := make(chan elevio.ButtonEvent)
 	drv_floors := make(chan int)
 	drv_obstr := make(chan bool)
@@ -60,40 +31,40 @@ func Get_livedata(
 	for {
 		select {
 		case a := <-drv_buttons:
-			elev_status[4] = a.Floor
-			elev_status[5] = int(a.Button)
-			elev_status_chan <- elev_status
+			my_status.buttonfloor = a.Floor
+			my_status.buttontype = int(a.Button)
+			elev_status_chan <- my_status
 
 		case a := <-drv_floors:
-			elev_status[4] = -1
-			elev_status[5] = -1
-			elev_status[1] = a
-			elev_status_chan <- elev_status
+			my_status.buttonfloor = -1
+			my_status.buttontype = -1
+			my_status.floor = a
+			elev_status_chan <- my_status
 
 		case a := <-drv_obstr:
-			elev_status[4] = -1
-			elev_status[5] = -1
+			my_status.buttonfloor = -1
+			my_status.buttontype = -1
 			if a {
-				elev_status[3] = 1
+				my_status.obstructed = true
 			} else {
-				elev_status[3] = 0
+				my_status.obstructed = false
 			}
-			elev_status_chan <- elev_status
+			elev_status_chan <- my_status
 
 		case a := <-direction:
-			elev_status[4] = -1
-			elev_status[5] = -1
-			elev_status[0] = int(a)
+			my_status.buttonfloor = -1
+			my_status.buttontype = -1
+			my_status.direction = int(a)
 
 		case a := <-door_open:
-			elev_status[4] = -1
-			elev_status[5] = -1
+			my_status.buttonfloor = -1
+			my_status.buttontype = -1
 			if a {
-				elev_status[2] = 1
+				my_status.doors = true
 			} else {
-				elev_status[2] = 0
+				my_status.doors = false
 			}
-			elev_status_chan <- elev_status
+			elev_status_chan <- my_status
 		}
 	}
 }
--- project/src/main.go
+++ project/src/main.go
@@ -11,17 +11,18 @@ func main() {
 
 	num_floors := 4
 
-	fmt.Println("Booting elevator")
+	fmt.Println("Booting elevator") // just to know we're running
 
-	elevio.Init("localhost:15657", num_floors)
+	elevio.Init("localhost:15657", num_floors) // connect to elevatorsimulator
 
-	my_status := make(chan []int)
+	my_status := make(chan elev_data.Elev_status) // need these for testing
 	my_dir := make(chan elevio.MotorDirection)
 	my_door := make(chan bool)
 
-	go elev_data.Get_livedata(my_status, my_dir, my_door)
-	go func() {
-		for {
+	go elev_data.Get_livedata(my_status, my_dir, my_door) // testing this
+
+	go func() { // these need to be updated when changing motordirection or door open/close()
+		for { // changing manually just to observe the statuschange
 			time.Sleep(time.Second * 5)
 
 			my_dir <- elevio.MD_Up
