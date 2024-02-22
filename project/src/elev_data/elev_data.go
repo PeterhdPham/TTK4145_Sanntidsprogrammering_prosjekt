c09fe9bf074c2f7fe622e17ff83b96a1826d9fc3
Einar <einarbergslid@gmail.com>
2024-02-22 13:56:43 +0100

    Finished first iteration of func Get_livedata( 	elev_status_chan chan<- []int, 	direction chan elevio.MotorDirection, 	door_open chan bool, )
    The function continuously updates the channel elev_status_chan with the current status of the elevator. Requires the direction and door-status to be sent here from outside the function

--- project/src/elev_data/elev_data.go
+++ project/src/elev_data/elev_data.go
@@ -2,11 +2,11 @@ package elev_data
 
 import "Driver-go/elevio"
 
-func get_livedata(
+func Get_livedata2(
 	chan1 chan<- elevio.ButtonEvent,
 	chan2 chan<- int,
-    chan3 chan<- bool,
-    chan4 chan<- bool,
+	chan3 chan<- bool,
+	chan4 chan<- bool,
 ) {
 
 	drv_buttons := make(chan elevio.ButtonEvent)
@@ -19,5 +19,81 @@ func get_livedata(
 	go elevio.PollObstructionSwitch(drv_obstr)
 	go elevio.PollStopButton(drv_stop)
 
-	select{}
+	for {
+		select {
+		case a := <-drv_buttons:
+			chan1 <- a
+
+		case a := <-drv_floors:
+			chan2 <- a
+
+		case a := <-drv_obstr:
+			chan3 <- a
+
+		case a := <-drv_stop:
+			chan4 <- a
+		}
+	}
+}
+
+func Get_livedata(
+	elev_status_chan chan<- []int,
+	direction chan elevio.MotorDirection,
+	door_open chan bool,
+) {
+
+	elev_status := []int{0, 0, 0, 0, 0, 0}
+	// Slice contains:[direction(-1,0,1), Floor I'm at (0,1,2,3...n), Doors open? (0,1), Door obstructed? (0,1)
+	// buttonfloor(0,1,2,3...n), buttontype(0,1,2)]
+	// buttontype 0 is up, type 1 is down, type 2 is hall call
+
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
+
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+
+	for {
+		select {
+		case a := <-drv_buttons:
+			elev_status[4] = a.Floor
+			elev_status[5] = int(a.Button)
+			elev_status_chan <- elev_status
+
+		case a := <-drv_floors:
+			elev_status[4] = -1
+			elev_status[5] = -1
+			elev_status[1] = a
+			elev_status_chan <- elev_status
+
+		case a := <-drv_obstr:
+			elev_status[4] = -1
+			elev_status[5] = -1
+			if a {
+				elev_status[3] = 1
+			} else {
+				elev_status[3] = 0
+			}
+			elev_status_chan <- elev_status
+
+		case a := <-direction:
+			elev_status[4] = -1
+			elev_status[5] = -1
+			elev_status[0] = int(a)
+
+		case a := <-door_open:
+			elev_status[4] = -1
+			elev_status[5] = -1
+			if a {
+				elev_status[2] = 1
+			} else {
+				elev_status[2] = 0
+			}
+			elev_status_chan <- elev_status
+		}
+	}
 }
--- project/src/main.go
+++ project/src/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"Driver-go/elevio"
 	"fmt"
-	// "time"
+	"project/elev_data"
+	"time"
 )
 
 func main() {
@@ -14,6 +15,27 @@ func main() {
 
 	elevio.Init("localhost:15657", num_floors)
 
-	elev_data.get_livedata()
+	my_status := make(chan []int)
+	my_dir := make(chan elevio.MotorDirection)
+	my_door := make(chan bool)
+
+	go elev_data.Get_livedata(my_status, my_dir, my_door)
+	go func() {
+		for {
+			time.Sleep(time.Second * 5)
+
+			my_dir <- elevio.MD_Up
+			my_door <- true
+
+			time.Sleep(time.Second * 5)
+
+			my_dir <- elevio.MD_Down
+			my_door <- false
+		}
+	}()
+
+	for status_update := range my_status {
+		fmt.Println("New updated status: ", status_update)
+	}
 
 }
