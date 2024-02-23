0371b54ef4c35f0c5ad844496cdbf8560a642fac
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2024-02-23 10:18:43 +0100

    Light Handling for elevator (#25)
    * commit
    
    * working light update function based on list of values
    
    * removed testning function
    
    * added a txt file of need code for main.go file
    
    * updated function names
    
    * changed to CamelCase formatting
    
    * testing added

--- project/src/main.go
+++ project/src/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "project/tcp"
+import (
+	"Driver-go/elevio"
+	"fmt"
+	"project/light_status"
+)
 
 func main() {
-	tcp.Config_Roles()
+	num_floors := 4
+
+	fmt.Println("Booting elevator")
+	elevio.Init("localhost:15657", num_floors)
+
+	updateLigthChan := make(chan light_status.LightStatus) // Create a channel for light status updates
+
+	// Start continuously updating light status based on channel updates
+	go light_status.ContinuousUpdate(updateLigthChan)
+
+	// Send the initial light status with all lights off through the channel
+	updateLigthChan <- light_status.InitLights(num_floors)
+
+	go light_status.RandomizeLights(num_floors, updateLigthChan)
+
+	select {}
 }
