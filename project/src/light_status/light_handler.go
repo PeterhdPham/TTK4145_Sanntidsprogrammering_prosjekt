558400e26887c216eb1744b4b932c430708e8ca3
Kristian Goystdal <krisgoy100@gmail.com>
2024-02-23 10:12:31 +0100

    changed to CamelCase formatting

--- project/src/light_status/light_handler.go
+++ project/src/light_status/light_handler.go
@@ -8,55 +8,55 @@ import (
 )
 
 type LightStatus struct {
-	hall_light_up   []bool `json:"hall_light_up"`
-	hall_light_down []bool `json:"hall_light_down"`
-	cab_light       []bool `json:"cab_light"`
+	HallLightUp   []bool `json:"HallLightUp"`
+	HallLightDown []bool `json:"HallLightDown"`
+	CabLight      []bool `json:"CabLight"`
 }
 
-func InitLights(number_of_floors int) LightStatus {
-	hallUp := make([]bool, number_of_floors)
-	hallDown := make([]bool, number_of_floors)
-	cab := make([]bool, number_of_floors)
+func InitLights(NumberOfFloors int) LightStatus {
+	hallUp := make([]bool, NumberOfFloors)
+	hallDown := make([]bool, NumberOfFloors)
+	cab := make([]bool, NumberOfFloors)
 
 	return LightStatus{
-		hall_light_up:   hallUp,
-		hall_light_down: hallDown,
-		cab_light:       cab,
+		HallLightUp:   hallUp,
+		HallLightDown: hallDown,
+		CabLight:      cab,
 	}
 }
 
 func UpdateLights(lightStatus LightStatus) { // Pass LightStatus as an argument
 	// Iterating through hall_light_up and changing the lights based on the values
-	for i, val := range lightStatus.hall_light_up {
+	for i, val := range lightStatus.HallLightUp {
 		elevio.SetButtonLamp(0, i, val)
 	}
 
 	// Iterating through hall_light_down and changing the lights based on the values
-	for i, val := range lightStatus.hall_light_down {
+	for i, val := range lightStatus.HallLightDown {
 		elevio.SetButtonLamp(1, i, val)
 	}
 
 	// Iterating through cab_light and changing the lights based on the values
-	for i, val := range lightStatus.cab_light {
+	for i, val := range lightStatus.CabLight {
 		elevio.SetButtonLamp(2, i, val)
 	}
 }
 
-func RandomizeLights(number_of_floors int, updateChan chan<- LightStatus) {
+func RandomizeLights(NumberOfFloors int, updateChan chan<- LightStatus) {
 	rand.Seed(time.Now().UnixNano()) // Seed random number generator
 
 	for {
-		lightStatus := InitLights(number_of_floors) // Reinitialize or create a new LightStatus
+		lightStatus := InitLights(NumberOfFloors) // Reinitialize or create a new LightStatus
 
 		// Randomly update the light status
-		for i := range lightStatus.hall_light_up {
-			lightStatus.hall_light_up[i] = rand.Intn(2) == 1
+		for i := range lightStatus.HallLightUp {
+			lightStatus.HallLightUp[i] = rand.Intn(2) == 1
 		}
-		for i := range lightStatus.hall_light_down {
-			lightStatus.hall_light_down[i] = rand.Intn(2) == 1
+		for i := range lightStatus.HallLightDown {
+			lightStatus.HallLightDown[i] = rand.Intn(2) == 1
 		}
-		for i := range lightStatus.cab_light {
-			lightStatus.cab_light[i] = rand.Intn(2) == 1
+		for i := range lightStatus.CabLight {
+			lightStatus.CabLight[i] = rand.Intn(2) == 1
 		}
 
 		updateChan <- lightStatus // Send the new LightStatus through the channel
@@ -67,7 +67,7 @@ func RandomizeLights(number_of_floors int, updateChan chan<- LightStatus) {
 
 func ContinuousUpdate(updateChan <-chan LightStatus) {
 	for newLightStatus := range updateChan {
-		UpdateLights(newLightStatus)                    // Apply the update to the lights
+		UpdateLights(newLightStatus)                     // Apply the update to the lights
 		fmt.Println("New light status:", newLightStatus) // Optional: print the new status for verification
 	}
 }
