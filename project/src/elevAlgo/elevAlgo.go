f043959b0fba4b0a2d96461b7103f24e273ffb8a
Kristian Goystdal <krisgoy100@gmail.com>
2024-03-10 18:35:43 +0100

    debug

--- project/src/elevAlgo/elevAlgo.go
+++ project/src/elevAlgo/elevAlgo.go
@@ -43,6 +43,7 @@ func ElevAlgo(masterList *elevData.MasterList, elevStatus chan elevData.ElevStat
 			}
 			myStatus.Buttonfloor = a.Floor
 			myStatus.Buttontype = int(a.Button)
+			fmt.Printf("Floor %d and Button %d\n", myStatus.Floor, myStatus.Buttontype)
 		case a := <-drvFloors:
 			myStatus = FSM_ArrivalAtFloor(myStatus, myOrders, a)
 		case a := <-drvObstr:
