21ccecfe0b3d91717fbf66cc412131d3afbfef8e
peterhdpham <peterhdpham@gmail.com>
2024-03-14 13:36:18 +0100

    modified:   project/src/udp/communication.go

--- project/src/udp/communication.go
+++ project/src/udp/communication.go
@@ -15,8 +15,8 @@ const PORT = "9999" // Port used to broadcast and listen to "I'm alive"-messages
 const BROADCAST_ADDR = "255.255.255.255:" + PORT // Address to broadcast "I'm alive"-msg
 const BROADCAST_PERIOD = 100 * time.Millisecond  // Time to wait before broadcasting new msg
 const LISTEN_ADDR = "0.0.0.0:" + PORT            // Address to listen for "I'm alive"-msg
-const LISTEN_TIMEOUT = 5 * time.Second           // Time to listen before giving up
-const NODE_LIFE = time.Second                    // Time added to node-lifetime when msg is received
+const LISTEN_TIMEOUT = 10 * time.Second          // Time to listen before giving up
+const NODE_LIFE = 5 * time.Second                // Time added to node-lifetime when msg is received
 
 func BroadcastLife() {
 
