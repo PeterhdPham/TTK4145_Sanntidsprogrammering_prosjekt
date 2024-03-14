750bf98aea2714f4c0af940bc5702d1aeaed1d19
Kristian Goystdal <krisgoy100@gmail.com>
2024-03-14 15:13:02 +0100

    proper handling of masterlist prev

--- project/src/tcp/role_config.go
+++ project/src/tcp/role_config.go
@@ -27,7 +27,7 @@ var (
 
 	WaitingForConfirmation bool              //
 	ServerActive           = make(chan bool) //Server state
-	ReceivedPrevMasterList bool // Master list that server receives from client that used to be server
+	ReceivedPrevMasterList bool              // Master list that server receives from client that used to be server
 )
 
 func Config_Roles(pointerElevator *defs.Elevator, masterElevator *defs.MasterList) {
@@ -291,7 +291,15 @@ func handleConnection(conn net.Conn, masterElevator *defs.MasterList) {
 				} else {
 					if ReceivedPrevMasterList {
 						fmt.Println("Server received masterList from previous server")
-						*masterElevator = v
+						if utility.IsIPInMasterList(defs.MyIP, v) {
+							*masterElevator = v
+							fmt.Println("Overwriting existing masterList")
+						} else {
+							masterElevator.Elevators = append(masterElevator.Elevators, v.Elevators...)
+							fmt.Println("Adding prev list to current masterList")
+						}
+						masterToSend := utility.MarshalJson(*masterElevator)
+						broadcast.BroadcastMessage(nil, masterToSend)
 					}
 					fmt.Println("Server did not receive the correct confirmation")
 				}
